80a6e752d2617a82f438053a48a9ec9c3721ae1d
Awinashkr31 <awinashkr31@gmail.com>
2026-07-31 19:19:17 +0530

    chore: generate Turbopack development build assets

--- frontend/check_images.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const products = await prisma.product.findMany({ include: { images: true }, orderBy: { createdAt: 'desc' }, take: 1 });
-  console.log(JSON.stringify(products, null, 2));
-}
-
-main().finally(() => prisma.$disconnect());
--- frontend/test.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const p = await prisma.product.findFirst();
-  console.log(p);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
